c2d572feaba43a9b33e55fc1c6338b7de37514fc
Jaron Long <harkonnen@yeah.net>
2019-08-13 11:29:46 +0800

    Update with successful Vue-cli demo.

---
+++ packages/xcrm-success-spa/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <test-module/>
+  </div>
+</template>
+
+<script>
+  import TestModule from './components/test-module.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      TestModule
+    }
+  }
+</script>
---
+++ packages/xcrm-success-spa/src/components/test-module.vue
@@ -0,0 +1,17 @@
+<template>
+  <test-module/>
+</template>
+
+<script>
+  import { TestModule } from '@xcrm-demo/modules'
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      TestModule
+    },
+    props: {
+      msg: String
+    }
+  }
+</script>
